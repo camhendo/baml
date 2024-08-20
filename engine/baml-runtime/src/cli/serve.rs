1e5983f386fac0fb961b43c7e16393eee7ee7385
Sam Lijin <sam@boundaryml.com>
2024-08-20 13:48:01 -0700

    continue server impl

--- engine/baml-runtime/src/cli/serve.rs
+++ engine/baml-runtime/src/cli/serve.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use aws_sdk_bedrockruntime::primitives::event_stream;
 use axum::{
     http::StatusCode,
     response::IntoResponse,
@@ -33,68 +32,123 @@ pub struct ServeArgs {
     no_version_check: bool,
 }
 
-// TODO: wasm32 bail-out
-
 impl ServeArgs {
     pub fn run(&self) -> Result<()> {
-        let baml_runtime = Arc::new(
-            BamlRuntime::from_directory(
-                &PathBuf::from("/Users/sam/baml/integ-tests/baml_src"),
-                std::env::vars().collect(),
-            )
-            .unwrap(),
-        );
-
-        let clone = self.clone();
-        let rt = tokio::runtime::Runtime::new()?;
-        rt.block_on(async move {
-            clone.run_async(baml_runtime.clone()).await;
+        let server = Arc::new(Server {
+            args: self.clone(),
+            t: tokio::runtime::Runtime::new()?,
+            b: BamlRuntime::from_directory(&PathBuf::from(&self.from), std::env::vars().collect())?,
         });
-        Ok(())
-    }
 
-    pub async fn run_async(&self, obaml_runtime: Arc<BamlRuntime>) -> Result<()> {
+        server.serve()
+    }
+}
+struct Server {
+    args: ServeArgs,
+    t: tokio::runtime::Runtime,
+    b: BamlRuntime,
+}
+
+impl Server {
+    pub fn serve(self: Arc<Self>) -> Result<()> {
         // build our application with a route
         let app = Router::new();
-        let baml_runtime = obaml_runtime.clone();
-        let app = app.route(
-            "/message/:msg",
-            get(|path| stream_jsonnl0(baml_runtime, path)),
-        );
-        let baml_runtime = obaml_runtime.clone();
-        let app = app.route(
-            "/stream_jsonnl/:msg",
-            get(|path| stream_jsonnl(baml_runtime, path)),
-        );
 
-        // run our app with hyper, listening globally on port 3000
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-        axum::serve(listener, app).await?;
-        Ok(())
+        let s = self.clone();
+        let app = app.route("/echo/:msg", get(move |path| s.echo(path)));
+
+        let s = self.clone();
+        // TODO: convert to SSE
+        let app = app.route("/stream/:msg", get(move |path| s.stream_jsonnl(path)));
+        // let baml_runtime = self.b.clone();
+        // let app = app.route(
+        //     "/stream_jsonnl/:msg",
+        //     get(|path| stream_jsonnl(baml_runtime, path)),
+        // );
+
+        self.t.block_on(async move {
+            let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+            axum::serve(listener, app).await?;
+
+            Ok(())
+        })
     }
-}
 
-#[derive(Debug, Clone, Serialize)]
-struct Message {
-    msg: String,
-}
+    fn create_message_stream0(
+        self: Arc<Self>,
+        msg: String,
+    ) -> Pin<Box<dyn Stream<Item = String> + Send>> {
+        use tokio_stream::StreamExt;
+        Box::pin(
+            futures::stream::iter(std::iter::repeat(msg).take(3))
+                .throttle(std::time::Duration::from_secs(1)),
+        )
+    }
 
-fn create_message_stream0(
-    baml_runtime: Arc<BamlRuntime>,
-    msg: String,
-) -> Pin<Box<dyn Stream<Item = Message> + Send>> {
-    use tokio_stream::StreamExt;
-    Box::pin(
-        futures::stream::iter(std::iter::repeat(Message { msg }).take(10))
-            .throttle(std::time::Duration::from_secs(1)),
-    )
-}
-async fn stream_jsonnl0(
-    baml_runtime: Arc<BamlRuntime>,
-    axum::extract::Path(msg): axum::extract::Path<String>,
-) -> impl IntoResponse {
-    log::info!("stream_jsonnl0 called {}", msg);
-    StreamBodyAs::json_nl(create_message_stream0(baml_runtime, msg))
+    async fn echo(
+        self: Arc<Self>,
+        axum::extract::Path(msg): axum::extract::Path<String>,
+    ) -> impl IntoResponse {
+        log::info!("stream_jsonnl0 called {}", msg);
+        StreamBodyAs::json_nl(self.create_message_stream0(msg))
+    }
+
+    fn create_message_stream(
+        self: Arc<Self>,
+        msg: String,
+    ) -> Pin<Box<dyn Stream<Item = BamlValue> + Send>> {
+        let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
+
+        log::info!("create_message_stream created {}", msg);
+        tokio::spawn(async move {
+            let mut args = IndexMap::new();
+            args.insert("input".to_string(), BamlValue::String(msg));
+            let ctx_mgr =
+                RuntimeContextManager::new_from_env_vars(std::env::vars().collect(), None);
+
+            let mut result_stream = self
+                .b
+                .stream_function(
+                    "PromptTestStreaming".to_string(),
+                    &args,
+                    &ctx_mgr,
+                    None,
+                    None,
+                )
+                .unwrap();
+            log::info!("result_stream created");
+
+            result_stream
+                .run(
+                    Some(move |result| {
+                        log::info!("on_event sending result");
+                        sender.send(result);
+                    }),
+                    &ctx_mgr,
+                    None,
+                    None,
+                )
+                .await;
+        });
+
+        let event_stream = EventStream { receiver };
+
+        Box::pin(event_stream)
+
+        // use tokio_stream::StreamExt;
+        // Box::pin(
+        //     futures::stream::iter(std::iter::repeat(Message { msg }).take(10))
+        //         .throttle(std::time::Duration::from_secs(1)),
+        // )
+    }
+
+    async fn stream_jsonnl(
+        self: Arc<Self>,
+        axum::extract::Path(msg): axum::extract::Path<String>,
+    ) -> impl IntoResponse {
+        log::info!("stream_jsonnl called {}", msg);
+        StreamBodyAs::json_nl(self.create_message_stream(msg))
+    }
 }
 
 fn create_message_stream(
